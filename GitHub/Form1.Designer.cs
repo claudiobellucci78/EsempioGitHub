6dd96e816c33a8b93880a45469b3fab32d65cd5d
Claudio Bellucci <claudio.bellucci@datacominformatica.it>
2018-05-22 12:03:23 +0200

    a
    222

--- GitHub/Form1.Designer.cs
+++ GitHub/Form1.Designer.cs
@@ -30,13 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-
             this.button1.Location = new System.Drawing.Point(12, 12);
-
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(149, 56);
             this.button1.TabIndex = 0;
@@ -53,11 +52,21 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Marianna";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(804, 484);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -71,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
